c758b45f80562b2704ab2b7e55ad6b26aa1bc432
LeeHungQNG <builequochung3004@gmail.com>
2025-03-30 10:35:55 +0700

    layout sidebar p2

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,21 +1,25 @@
+import { menuItem } from '@/constants';
+
 const Sidebar = () => {
   return (
     <div className="p-5 border-r border-r-gray-200 ">
       <a href="/" className="font-bold text-3xl inline-block mb-5">
         Ucademy
       </a>
-      <ul>
-        <MenuItem url="/" title="Khu vực học tập" />
-        <MenuItem url="/explore" title="Khám phá" />
+      <ul className="flex flex-col gap-2">
+        {menuItem.map((menu, index) => {
+          return <MenuItem key={index} url={menu.url} title={menu.title} icon={menu.icon}></MenuItem>;
+        })}
       </ul>
     </div>
   );
 };
 
-function MenuItem({ url = '/', title = '' }: { url: string; title: string }) {
+function MenuItem({ url = '/', title = '', icon }: { url: string; title: string; icon?: React.ReactNode }) {
   return (
     <li>
-      <a href={url} className="p-3 rounded-md flex items-center">
+      <a href={url} className="p-3 rounded-md flex items-center gap-3 hover:text-primary hover:bg-primary hover:bg-opacity-10 transition-all">
+        {icon}
         {title}
       </a>
     </li>
---
+++ src/constants/index.tsx
@@ -0,0 +1,15 @@
+import IconExplore from '@/components/icons/IconExplore';
+import IconPlay from '@/components/icons/IconPlay';
+
+export const menuItem: { url: string; title: string; icon: React.ReactNode }[] = [
+  {
+    url: '/',
+    title: 'Khu vực học tập',
+    icon: <IconPlay className="size-5" />,
+  },
+  {
+    url: '/explore',
+    title: 'Khám phá',
+    icon: <IconExplore className="size-5" />,
+  },
+];
