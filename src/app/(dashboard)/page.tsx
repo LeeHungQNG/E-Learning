c946bb19566a6bf13825249489100fd75a87309e
LeeHungQNG <builequochung3004@gmail.com>
2025-04-08 20:14:45 +0700

    redeploy webhook

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,15 +1,9 @@
 import { CourseGrid } from '@/components/common';
 import CourseItem from '@/components/course/CourseItem';
 import Heading from '@/components/typography/Heading';
-import createUser from '@/lib/actions/user.actions';
+
 
 const page = async () => {
-  const user = await createUser({
-    clerkId: 'clerk_123',
-    email: 'John101@gmail.com',
-    username: 'John Wick',
-  });
-
   return (
     <div>
       <Heading>Khám phá</Heading>
--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,11 +1,14 @@
+'use client';
 import { menuItem } from '@/constants';
-
 import { TMenuItem } from '../../types';
 import { ActiveLink } from '../common';
-import { UserButton } from '@clerk/nextjs';
+import { useAuth, UserButton } from '@clerk/nextjs';
 import { ModeToggle } from '../common/ModeToggle';
+import Link from 'next/link';
+import { IconMember } from '../icons';
 
 const Sidebar = () => {
+  const { userId } = useAuth();
   return (
     <div className="p-5 border-r border-r-gray-200 dark:border-opacity-10 bg-white dark:bg-grayDarker flex flex-col">
       <a href="/" className="font-bold text-3xl inline-block mb-5">
@@ -17,7 +20,19 @@ const Sidebar = () => {
         })}
       </ul>
       <div className="mt-auto flex gap-5 justify-between">
-        <UserButton />
+        {!userId ? (
+          <Link href={'/sign-in'} className="size-10 rounded-lg bg-primary text-white flex items-center justify-center p-1">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
+              />
+            </svg>
+          </Link>
+        ) : (
+          <UserButton />
+        )}
         <ModeToggle />
       </div>
     </div>
